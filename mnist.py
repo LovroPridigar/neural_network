16921368ee66ea85657bcb93751d7351214e5eff
lovro <lovro.pridigar@gmail.com>
2024-05-21 18:35:47 +0200

    Vektorizacija

--- mnist.py
+++ mnist.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 #Pripravimo primere za treniraje in testiranje
 mnist = tf.keras.datasets.mnist
 #(primeri za trening), (primeri za testiranje)
-(img_train, label_train), (img_test, label_test) = mnist.load_data()
+(img_train, label_train), (img_test, lbl_test) = mnist.load_data()
 
 #Normaliziramo vhodne vektorje, na začetku so elementi števila med 0-250
 img_train = tf.keras.utils.normalize(img_train, axis=1)
@@ -12,17 +12,27 @@ img_test = tf.keras.utils.normalize(img_test, axis=1)
 
 images = [] #To je dejanski list vhodnih vektorjev za treniranje
 images_test = [] #Ta je za testiranje
+label = []
+label_test = []
 
 #Podatke, ki smo dobili iz mnist data baze popravimo, da so bolj primerni za racunanje
-for example in img_train:
-    l = np.concatenate(example, axis=None) #pretvori vektor iz 28*28 v 784*1
+for img, lbl in zip(img_train, label_train):
+    l = np.concatenate(img, axis=None) #pretvori vektor iz 28*28 v 784*1
     l.shape = (784, 1) #popravi dimenzije
     images.append(l)
 
+    vector = np.zeros((10,1)) #Vektoriziramo število primera
+    vector[lbl] = lbl
+    label.append(vector)
+
 #Enako še za testne primere
-for example in img_test:
-    l = np.concatenate(example, axis=None) 
+for img, lbl in zip(img_test, lbl_test):
+    l = np.concatenate(img, axis=None) 
     l.shape = (784, 1)
     images_test.append(l)
+    
+    vector = np.zeros((10,1))
+    vector[lbl] = lbl
+    label.append(vector)
 
-data = (images, label_train), (images_test, label_test)
+data = (images, label), (images_test, label_test)
--- network.py
+++ network.py
@@ -19,16 +19,46 @@ for i in range(size-1):
 def sigmoid(z):
    return 1/(1 + np.exp(-z))
 
+def dsigmoid(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
 class Network(object):
-    def __init__(self):
+    def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
     #Za vektor dražljaja velikosti 729 vrne odgovor nevronske mreže
-    def Evaluate(self, stimulus):
+    def Eval(self, stimulus):
+        a = []
+        z = []
         for i in range(size-1):
             w, b = weights[i], bias[i]
+            z.append(np.dot(w, stimulus) + b)
             stimulus = sigmoid(np.dot(w, stimulus) + b)
-        return stimulus
+            a.append(stimulus)
+        return a, z
+    
+    def Evaluate(self, stimulum):
+        a, z = self.Eval(stimulum)
+        return a[-1]
     
     def CostFunction(self, input, result):
         return np.sum(np.square(self.Evaluate(input) - result))
+    
+    def Gradient(self, stimulus, result):
+        a, z = self.Eval(stimulus)
+        w = [np.zeros((784, 32)), np.zeros((32, 10))]
+        b = [np.zeros((32, 1)), np.zeros((10, 1))]
+        for L in range(size-2, -1, -1):
+            if L == 1:
+                for j in range(0, 10):
+                    b[L][j] = 2*(sigmoid(z[L][j]) - result[j]) * dsigmoid(z[j])
+                    for k in range(0, 32):
+                        w[L][k, j] = 2*(sigmoid(z[L][j]) - result[j]) * dsigmoid(z[j]) * a[L-1][k]
+            else:
+                pass
+        return w, b
+
+N = Network(weights, bias)
+stim = images[0]
+print(np.shape(label_train[0]))
+print(N.Gradient(stim, label_train[0]))
